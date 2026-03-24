f600131f6c99f7b24cfd9ee30cf4304881241764
FilippiAutomotores <filippiautomotores@gmail.com>
2026-03-23 22:19:08 -0300

    agregar PWA y mejoras mobile

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'biblioteca-v1';
+const FILES = [
+  '/biblioteca/',
+  '/biblioteca/index.html',
+  '/biblioteca/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
